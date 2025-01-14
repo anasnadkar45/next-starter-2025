a543b9c0546cf28a4ccc5fd0cc2d63055b90e80e
anasnadkar45 <anasnadkar23@gmail.com>
2025-01-14 21:04:11 +0530

    Updating Codebase

---
+++ src/app/utils/auth.ts
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth"
+import Nodemailer from "next-auth/providers/nodemailer"
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import Google from "next-auth/providers/google"
+import GitHub from "next-auth/providers/github"
+import prisma from "./db";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        Nodemailer({
+            server: {
+                host: process.env.EMAIL_SERVER_HOST,
+                port: process.env.EMAIL_SERVER_PORT,
+                auth: {
+                    user: process.env.EMAIL_SERVER_USER,
+                    pass: process.env.EMAIL_SERVER_PASSWORD,
+                },
+            },
+            from: process.env.EMAIL_FROM,
+        }),
+        Google,
+        GitHub,
+    ],
+    pages: {
+        verifyRequest: "/verify",
+    },
+})
